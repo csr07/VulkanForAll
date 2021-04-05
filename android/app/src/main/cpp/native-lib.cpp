54234f25ce9b800442ef13cd27973dcf754194e5
cesar.gomez <csrgmz@gmail.com>
2021-04-05 10:49:51 -0500

    First Attemp to send the Kotlin Surface to C++ through JNI, acomplished

--- android/app/src/main/cpp/native-lib.cpp
+++ android/app/src/main/cpp/native-lib.cpp
@@ -1,10 +1,37 @@
 #include <jni.h>
+//#include <android/native_activity.h>
+#include <android/native_window.h>
+#include <android/native_window_jni.h>
 #include <string>
+#include <android/log.h>
+
+static ANativeWindow* natWin = {0};
+static jobject globalSurfaceView = nullptr;
+
+static int32_t width = {640};
+static int32_t height = {480};
+
+void AndroidGetWindowSize(){
+    width = ANativeWindow_getWidth(natWin);
+    height = ANativeWindow_getHeight(natWin);
+}
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_csr_hellotriangle_MainActivity_stringFromJNI(
         JNIEnv* env,
         jobject /* this */) {
-    std::string hello = "Hello from C++";
+    std::string hello = "CSR: Hello from C++";
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_csr_hellotriangle_MainActivity_passSurfaceViewJNI( JNIEnv* env,
+                                    jobject obj,
+                                    jobject surfaceView ){
+
+    globalSurfaceView = env->NewGlobalRef(surfaceView);
+    natWin = ANativeWindow_fromSurface(env, globalSurfaceView);
+    AndroidGetWindowSize();
+    ANativeWindow_release(natWin);
+    return;
 }
--- android/app/src/main/java/com/csr/hellotriangle/MainActivity.kt
+++ android/app/src/main/java/com/csr/hellotriangle/MainActivity.kt
@@ -3,14 +3,50 @@ package com.csr.hellotriangle
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.view.Surface
+import android.view.SurfaceHolder
+import android.view.SurfaceView
+import android.view.View
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), SurfaceHolder.Callback {
+
+    private var surfaceHolder: SurfaceHolder? = null
+    private var surfaceView : SurfaceView? = null
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
         // Example of a call to a native method
         findViewById<TextView>(R.id.sample_text).text = stringFromJNI()
+
+        //CSR Manipulating SurfaceView and SurfaceHolder
+        surfaceView = findViewById(R.id.surfaceView)
+        setupSurfaceHolder()
+    }
+
+    private fun setViewVisibility(id: Int, visibility: Int) {
+        val view = findViewById<View>(id)
+        view!!.visibility = visibility
+    }
+
+    private fun setupSurfaceHolder(){
+        setViewVisibility(R.id.surfaceView, View.VISIBLE)
+        surfaceHolder = surfaceView!!.holder
+        surfaceHolder!!.addCallback(this)
+    }
+
+    override fun surfaceCreated(holder: SurfaceHolder) {
+        print("Surface Created!")
+        surfaceHolder?.surface?.let { passSurfaceViewJNI(it) }
+    }
+
+    override fun surfaceChanged(holder: SurfaceHolder, format: Int, width: Int, height: Int) {
+        //TODO("Not yet implemented")
+    }
+
+    override fun surfaceDestroyed(holder: SurfaceHolder) {
+        //TODO("Not yet implemented")
     }
 
     /**
@@ -18,6 +54,7 @@ class MainActivity : AppCompatActivity() {
      * which is packaged with this application.
      */
     external fun stringFromJNI(): String
+    external fun passSurfaceViewJNI(mySurface: Surface)
 
     companion object {
         // Used to load the 'native-lib' library on application startup.
